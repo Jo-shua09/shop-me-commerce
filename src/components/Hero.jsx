e246f82d64f54c501c61d4d66ca235d5247a8ee5
Joshua Onyeka <joshuaonyeka039@gmail.com>
2025-01-04 12:52:46 +0100

    commit 3

---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const Hero = () => {
+  return (
+    <div className="relative overflow-hidden  border-gray-900 mt-20 min-h-[550px] sm:min-h-[700px] bg-gray-100">
+      {/* background pattern */}
+      <div className="h-[700px] overflow-hidden w-[700px] bg-primary/40  absolute -top-1/2 right-0 rounded-3xl rotate-45 "></div>
+
+      {/*  hero section */}
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default Hero;
