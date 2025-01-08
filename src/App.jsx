113275a26004989aa93c72e558f32a90ae19d300
Joshua Onyeka <joshuaonyeka039@gmail.com>
2025-01-08 23:57:52 +0100

    review

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import Testimonials from "./components/Testimonials";
 function App() {
   React.useEffect(() => {
     AOS.init({
+      offset: 100,
       duration: 800,
       easing: "ease-in-out",
       delay: 100,
